02432478e076e2a5ecbfc124584b10034ac266e1
Michael Yankelev <myankelev@gmail.com>
2019-04-20 06:07:18 +0200

    add transaction history component

--- Webapp/app/containers/TransactionHistoryContainer/index.tsx
+++ Webapp/app/containers/TransactionHistoryContainer/index.tsx
@@ -7,6 +7,7 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { compose, Dispatch } from 'redux';
+import TransactionHistory from 'components/TransactionHistory';
 
 interface OwnProps {}
 
@@ -15,7 +16,7 @@ interface DispatchProps {}
 type Props = DispatchProps & OwnProps;
 
 const TransactionHistoryContainer: React.SFC<Props> = (props: Props) => {
-  return <Fragment>TransactionHistory</Fragment>;
+  return <TransactionHistory transactions={[]} />;
 };
 
 function mapDispatchToProps(dispatch: Dispatch) {
